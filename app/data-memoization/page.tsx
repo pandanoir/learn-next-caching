865f7da949e5c2ad31f6ef872d1644f8d3964130
pandanoir <pandanoir@ymail.ne.jp>
2025-04-21 12:11:58 +0900

    first

---
+++ app/data-memoization/page.tsx
@@ -0,0 +1,76 @@
+import { revalidateTag } from 'next/cache';
+import * as v from 'valibot';
+
+const UuidSchema = v.object({ uuid: v.string() });
+
+// 5秒間だけキャッシュする
+const fetchUuidWithTimeBasedRevalidation = () =>
+  fetch(`http://localhost:${process.env.PORT}/api/uuid#time-based`, {
+    next: { revalidate: 5 },
+  })
+    .then((res) => res.json())
+    .then((res) => v.parse(UuidSchema, res).uuid);
+
+// 永続的にキャッシュする
+const fetchUuidWithDataCache = () =>
+  fetch(`http://localhost:${process.env.PORT}/api/uuid#eternal-cached`, {
+    cache: 'force-cache',
+  })
+    .then((res) => res.json())
+    .then((res) => v.parse(UuidSchema, res).uuid);
+
+// 手動でrevalidate指示がくるまでキャッシュする (on-demand)
+const fetchUuidWithTag = () =>
+  fetch(`http://localhost:${process.env.PORT}/api/uuid#manually`, {
+    cache: 'force-cache',
+    next: { tags: ['tag'] },
+  })
+    .then((res) => res.json())
+    .then((res) => v.parse(UuidSchema, res).uuid);
+
+export default async function DataMemoization() {
+  const [uuidWithTimeLimitedCache, cachedUuid, uuidWithTaggedCache] =
+    await Promise.all([
+      fetchUuidWithTimeBasedRevalidation(),
+      fetchUuidWithDataCache(),
+      fetchUuidWithTag(),
+    ]);
+
+  return (
+    <div>
+      <h1>Data memoization</h1>
+      <ul>
+        <li>
+          uuid with time limited cache(valid for 5 seconds):{' '}
+          {uuidWithTimeLimitedCache}
+        </li>
+        <li>
+          revalidatable cached uuid: {uuidWithTaggedCache}
+          <button
+            type="button"
+            onClick={async () => {
+              'use server';
+              revalidateTag('tag');
+            }}
+          >
+            revalidate
+          </button>
+        </li>
+        <li>cached uuid: {cachedUuid}</li>
+      </ul>
+      <p>
+        <code>{`{ next: { revalidate: 5 } }`}</code>
+        が設定されたリクエストは5秒間だけキャッシュが使われる。5秒経過してからページをリロードすると新しい値になる。
+      </p>
+      <p>
+        <code>{`{ next: { tags: ['tag'] } }`}</code>
+        が設定されたリクエストはrevalidateTag('tag')を行うとrevalidateされる。ただし、このオプションをつけただけではキャッシュされないので、
+        <code>cache: 'force-cache'</code>もつける必要がある。
+      </p>
+      <p>
+        <code>{`{ cache: 'force-cache' }`}</code>
+        が設定されたリクエストはずっとキャッシュされ、ページをリロードしても同じ値のまま。
+      </p>
+    </div>
+  );
+}
---
+++ app/request-memoization/page.tsx
@@ -0,0 +1,31 @@
+import * as v from 'valibot';
+
+const UuidSchema = v.object({ uuid: v.string() });
+
+// サーバーリクエストの間だけ有効なキャッシュ
+const fetchUuid = () =>
+  fetch(`http://localhost:${process.env.PORT}/api/uuid`, { cache: 'no-cache' })
+    .then((res) => res.json())
+    .then((res) => v.parse(UuidSchema, res).uuid);
+
+export default async function RequestMemoization() {
+  const [uuid1, uuid2, uuid3] = await Promise.all([
+    fetchUuid(),
+    fetchUuid(),
+    fetchUuid(),
+  ]);
+
+  return (
+    <div>
+      <h1>Request memoization</h1>
+      <ul>
+        <li>uuid1: {uuid1}</li>
+        <li>uuid2: {uuid2}</li>
+        <li>uuid3: {uuid3}</li>
+      </ul>
+      <p>
+        urlとオプションが同一のfetchは、サーバーリクエストの間同じキャッシュが利用される。そのためuuid1、uuid2、uuid3はすべて同一の値が入ってくる。また、ページをリロードすると新しい値になる。
+      </p>
+    </div>
+  );
+}
